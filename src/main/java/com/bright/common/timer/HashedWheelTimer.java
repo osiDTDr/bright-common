f53cb2b8b15db796152f85c8e7b28cf56939f5bc
郑远 <zhengyuan@192.168.0.101>
2020-12-06 20:34:38 +0800

    add timer comment

--- src/main/java/com/bright/common/timer/HashedWheelTimer.java
+++ src/main/java/com/bright/common/timer/HashedWheelTimer.java
@@ -1,7 +1,10 @@
 package com.bright.common.timer;
 
 /**
- * 时钟轮算法
+ * 时钟轮算法。
+ * 1.常规的JDK 的 java.util.Timer 和 ScheduledExecutorService 等工具类，可实现简单的定时任务，底层用的是堆数据结构，
+ * 存取复杂度都是 O(nlog(n))，无法支撑海量定时任务。
+ * 2.而在定时任务量大、性能要求高的场景，为将任务存取及取消操作时间复杂度降为 O(1)，会使用时间轮方案。
  *
  * @author zhengyuan
  * @since 2020/12/06
